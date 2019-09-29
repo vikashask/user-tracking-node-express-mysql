b79a8a8dad0676962b4be5e95191192095406cd6
vikash <vikashraj144@gmail.com>
2019-09-29 23:20:14 +0530

    utils added

---
+++ utils/message.js
@@ -0,0 +1,18 @@
+module.exports = {
+    success:{
+        LOGIN:'Login successful',
+        REGISTER:'Register successful',
+        PROFILE_UPDATED:'Profile updated successful',
+        FORGOT_PASSWORD_SENT:'Forgot password sent successful',
+        LOGOUT:'Logout successful',
+    },
+    error:{
+        INVALID_LOGIN:'Invalid login credentials',
+        UNABLE_REGISTER:'Unable to register',
+        USER_ALLREADY_EXIST:'User already exist',
+        PASSWORD_NOT_MATCHED:'Password did not matched',
+        USER_NOT_EXIST:'User not exist',
+        SOMETHING_WRONG:'Something went wrong',
+    },
+
+};
