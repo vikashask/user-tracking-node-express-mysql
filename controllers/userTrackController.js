4db95121b0bdbd5cdc72707bbe56e19646f445cb
vikash <vikashraj144@gmail.com>
2019-10-16 16:56:21 +0530

    page created

--- controllers/userTrackController.js
+++ controllers/userTrackController.js
@@ -1,2 +1,14 @@
 var knex = require('../config/knex');
 var message = require('../utils/message');
+
+module.exports.getAllTrackingDetails = async (req, res) => {
+    try {
+      var data = await knex.select().table('table name');
+      console.log("---", JSON.parse(JSON.stringify(data))[0]);
+      return res.json({
+        response: JSON.parse(JSON.stringify(data))[0]
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
